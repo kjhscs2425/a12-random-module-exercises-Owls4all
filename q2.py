f1ca32b5bc6662063307c97447c854db1193d84a
owls4all <owls4all@gmail.com>
2025-01-29 12:37:04 -0800

    made progress on q2

--- q2.py
+++ q2.py
@@ -1,24 +1,32 @@
 import random as r
 # Make a random pet.
-
+type=""
+age=""
+weight=""
 # Choose:
 # Type of animal (at least 3 choices, string)
 animal = r.choice(["bear","wolf","horse"])
 # Age (integer)
 if animal == 'bear':
-    type = r.choice["polar","brown","black","grizzly"]
+    type = r.choice(["polar","brown","black","grizzly"])
     if type == 'polar':
         age = str(r.randint(0,30))+"-year-old"
         weight = str(r.uniform(300,800))+" kilograms"
-    if type == 'brown':
+    if type == 'black':
         age = str(r.randint(0,44))+"-year-old"
         weight = str(r.uniform(41,250))+" kilograms"
+    if type == 'brown':
+        age = str(r.randint(0,47))+"-year-old"
+        weight = str(r.uniform(80,600))+" kilograms"
+    if type == 'grizzly':
+        age = str(r.randint(0,44))+"-year-old"
+        weight = str(r.uniform(95,389))+" kilograms"
 if animal == 'wolf':
-    type = r.choice([])
-    if type == '':
-        pass
+    type = r.choice(["silver","arctic","golden"])
+    age = str(r.randint(0,13))+"-year-old"
+    weight=str(r.uniform(12,79))+" kilograms"
 if animal == 'horse':
-    type = r.choice([])
+    type = r.choice(["something"])
 
 # Print a summary of your pet using an f-string
 print(f"Your pet is a {age} {type} {animal}, weighing {weight}.")#REPLACE THIS WITH YOUR CODE
