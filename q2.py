7b8200441d01b3a22393529d30e7d8ee156190f2
owls4all <owls4all@gmail.com>
2025-01-29 12:17:28 -0800

    began q2

--- q2.py
+++ q2.py
@@ -1,14 +1,24 @@
+import random as r
 # Make a random pet.
 
 # Choose:
 # Type of animal (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
+animal = r.choice(["bear","wolf","horse"])
 # Age (integer)
-age = None#REPLACE THIS WITH YOUR CODE
-# Color (at least 3 choices, string)
-animal = None#REPLACE THIS WITH YOUR CODE
-# Weight (float)
-weight = None#REPLACE THIS WITH YOUR CODE
+if animal == 'bear':
+    type = r.choice["polar","brown","black","grizzly"]
+    if type == 'polar':
+        age = str(r.randint(0,30))+"-year-old"
+        weight = str(r.uniform(300,800))+" kilograms"
+    if type == 'brown':
+        age = str(r.randint(0,44))+"-year-old"
+        weight = str(r.uniform(41,250))+" kilograms"
+if animal == 'wolf':
+    type = r.choice([])
+    if type == '':
+        pass
+if animal == 'horse':
+    type = r.choice([])
 
 # Print a summary of your pet using an f-string
-print(f"Your pet is a {animal}")#REPLACE THIS WITH YOUR CODE
+print(f"Your pet is a {age} {type} {animal}, weighing {weight}.")#REPLACE THIS WITH YOUR CODE
